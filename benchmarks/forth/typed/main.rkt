587975765f20d3f408b164197325f5e17f0bd9a1
Spenser Bauman <sabauma@gmail.com>
2016-03-07 22:58:05 -0500

    Forth updates

--- benchmarks/forth/typed/main.rkt
+++ benchmarks/forth/typed/main.rkt
@@ -7,6 +7,13 @@
   (forth-eval* (-> Input-Port (Values Any Any)))
 )
 
+(define N
+ (let ([args (current-command-line-arguments)])
+   (if (< (vector-length args) 1) 1
+     (let ([n (string->number (vector-ref args 0))])
+       (if (fixnum? n) n
+         (error 'main "must have a fixnum argument"))))))
+
 ;; =============================================================================
 
 (define (main)
@@ -15,4 +22,5 @@
       (let-values ([(_e _s) (forth-eval* p)]) (void))))
   (void))
 
+(for ([i (in-range (sub1 N))]) (main))
 (time (main))
